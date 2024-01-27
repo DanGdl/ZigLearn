939973a19816b62d8024f3cfa8836812693eb6e8
DanDdl <dangladste@gmail.com>
2024-01-27 19:46:54 +0200

    [anagram] remove allocations while swapping, support swap for symbols with different sizes

--- 01_Anagram/src/main.zig
+++ 01_Anagram/src/main.zig
@@ -71,11 +71,8 @@ pub fn main() !void {
         }
 
         if (Utf8View.init(filter)) |filter_utf| {
-            if (makeAnagram(input, filter_utf)) {
-                try stdout.print("Result: {s}\nTo exit enter q/Q, else - \"Enter\": \n", .{input});
-            } else |err| {
-                try stdout.print("Failed to make anagram: {}\n", .{err});
-            }
+            makeAnagram(input, filter_utf);
+            try stdout.print("Result: {s}\nTo exit enter q/Q, else - \"Enter\": \n", .{input});
             try bw.flush();
         } else |err| {
             std.log.err("Failed to convert input to UTF-8: {}\n", .{err});
@@ -106,7 +103,7 @@ pub fn main() !void {
     }
 }
 
-fn makeAnagram(input: []u8, filter: Utf8View) !void {
+fn makeAnagram(input: []u8, filter: Utf8View) void {
     var prev_start: usize = 0;
     var start: usize = 0;
     var end: usize = 0;
@@ -120,25 +117,25 @@ fn makeAnagram(input: []u8, filter: Utf8View) !void {
         }
         start = prev_start;
         end = i;
-        try reverseWord(input[start..end], filter);
+        reverseWord(input[start..end], filter);
         prev_start = i + 1;
         i += l;
     }
     start = prev_start;
     end = input.len;
-    try reverseWord(input[start..end], filter);
+    reverseWord(input[start..end], filter);
 }
 
-fn reverseWord(input: []u8, filter: Utf8View) !void {
+fn reverseWord(input: []u8, filter: Utf8View) void {
     if (input.len == 0) {
         return;
     }
     var end = input.len - 1;
     var start: usize = 0;
     while (start < end) {
-        const len_start: usize = unicode.utf8ByteSequenceLength(input[start]) catch 0;
+        const len_start: u3 = unicode.utf8ByteSequenceLength(input[start]) catch 0;
         while (end >= 0) : (end -= 1) {
-            const rc: usize = unicode.utf8ByteSequenceLength(input[end]) catch 0;
+            const rc: u3 = unicode.utf8ByteSequenceLength(input[end]) catch 0;
             if (rc != 0) {
                 break;
             }
@@ -146,44 +143,63 @@ fn reverseWord(input: []u8, filter: Utf8View) !void {
         if (start > end) {
             break;
         }
-        const len_end: usize = unicode.utf8ByteSequenceLength(input[end]) catch 0;
+        const len_end: u3 = unicode.utf8ByteSequenceLength(input[end]) catch 0;
 
         if (isIgnored(input[start .. start + len_start], filter)) {
             start += len_start;
         } else if (isIgnored(input[end .. end + len_end], filter)) {
             end -= len_end;
         } else {
-            if (len_start == 1 and len_end == 1) {
-                var tmp = input[start];
-                input[start] = input[end];
-                input[end] = tmp;
+            const size_symbol_buff: usize = 16; // max size of tmp buffer for biggest symbol, bytes
+            var size_tmp_symbol: usize = 0; // size of biggest symbol to swap, bytes
+            var size_symbol: usize = 0; // size of smallest symbol to swap, bytes
+            var idx_tmp_from_s: usize = 0; // index of biggest symbol's start in array 'input'
+            var idx_from_s: usize = 0; // index of smallest symbol's start in array 'input'
+            var idx_into_s: usize = 0; // index in array 'input' where to put smallest symbol
+            var idx_tmp_into_s: usize = 0; // index in array 'input' where to put tmp symbol
+            var buff_tmp = [_]u8{0} ** size_symbol_buff;
+
+            var idx_start: usize = 0;
+            var idx_end: usize = 0;
+            const step: usize = 1;
+            if (len_start >= len_end) {
+                idx_start = start + len_start;
+                idx_end = end;
+
+                size_tmp_symbol = len_start;
+                size_symbol = len_end;
+
+                idx_tmp_from_s = start;
+                idx_into_s = start;
+                idx_from_s = end;
+                idx_tmp_into_s = end + len_end - len_start;
             } else {
-                const allocator = std.heap.page_allocator;
-                var first = try allocator.alloc(u8, len_start);
-                std.mem.copy(u8, first, input[start .. start + len_start]);
+                idx_start = end + len_end - len_start - 1;
+                idx_end = start + len_start;
 
-                var last = try allocator.alloc(u8, len_end);
-                std.mem.copy(u8, last, input[end .. end + len_end]);
+                size_tmp_symbol = len_end;
+                size_symbol = len_start;
 
-                var tmp = try allocator.alloc(u8, end - start - len_start);
-                std.mem.copy(u8, tmp, input[start + len_start .. end]);
-
-                var s = start;
-                var e = start + len_end;
-                std.mem.copy(u8, input[s..e], last);
-
-                s = e;
-                e = e + tmp.len;
-                std.mem.copy(u8, input[s..e], tmp);
-
-                s = e;
-                e = e + len_start;
-                std.mem.copy(u8, input[s..e], first);
-
-                allocator.free(first);
-                allocator.free(last);
-                allocator.free(tmp);
+                idx_tmp_from_s = end;
+                idx_into_s = end + len_end - len_start;
+                idx_from_s = start;
+                idx_tmp_into_s = start;
             }
+            std.mem.copy(u8, &buff_tmp, input[idx_tmp_from_s..(idx_tmp_from_s + size_tmp_symbol)]);
+            std.mem.copy(u8, input[idx_into_s..(idx_into_s + size_symbol)], input[idx_from_s..(idx_from_s + size_symbol)]);
+            if (len_start != len_end) {
+                while (idx_start != idx_end) {
+                    var next: usize = 0;
+                    if (len_start >= len_end) {
+                        next = idx_start + step;
+                    } else {
+                        next = idx_start - step;
+                    }
+                    input[idx_start] = input[next];
+                    idx_start = next;
+                }
+            }
+            std.mem.copy(u8, input[idx_tmp_into_s..(idx_tmp_into_s + size_tmp_symbol)], buff_tmp[0..size_tmp_symbol]);
             start += len_end;
             end -= len_start;
         }
@@ -208,6 +224,19 @@ fn isIgnored(symbol: []const u8, filter: Utf8View) bool {
     }
 }
 
+test "test_UTF8_diff_size" {
+    const filter = try Utf8View.init("");
+    const in: [:0]const u8 = "Boлшeбный 1 кpoлик";
+    const ex: [:0]const u8 = "йынбeшлoB 1 килopк";
+
+    var input: [in.len:0]u8 = undefined;
+    var exp: [ex.len:0]u8 = undefined;
+    std.mem.copy(u8, &input, in);
+    std.mem.copy(u8, &exp, ex);
+    makeAnagram(&input, filter);
+    try std.testing.expectEqual(exp, input);
+}
+
 test "test_ASCII_no_filter" {
     const filter = try Utf8View.init("");
     const in: [:0]const u8 = "Foxminded cool 24/7";
@@ -217,7 +246,7 @@ test "test_ASCII_no_filter" {
     var exp: [ex.len:0]u8 = undefined;
     std.mem.copy(u8, &input, in);
     std.mem.copy(u8, &exp, ex);
-    try makeAnagram(&input, filter);
+    makeAnagram(&input, filter);
     try std.testing.expectEqual(exp, input);
 }
 
@@ -230,7 +259,7 @@ test "test_ASCII_filter" {
     var exp: [ex.len:0]u8 = undefined;
     std.mem.copy(u8, &input, in);
     std.mem.copy(u8, &exp, ex);
-    try makeAnagram(&input, filter);
+    makeAnagram(&input, filter);
     try std.testing.expectEqual(exp, input);
 }
 
@@ -243,7 +272,7 @@ test "test_UTF8_no_filter" {
     var exp: [ex.len:0]u8 = undefined;
     std.mem.copy(u8, &input, in);
     std.mem.copy(u8, &exp, ex);
-    try makeAnagram(&input, filter);
+    makeAnagram(&input, filter);
     try std.testing.expectEqual(exp, input);
 }
 
@@ -256,7 +285,7 @@ test "test_UTF8_filter" {
     var exp: [ex.len:0]u8 = undefined;
     std.mem.copy(u8, &input, in);
     std.mem.copy(u8, &exp, ex);
-    try makeAnagram(&input, filter);
+    makeAnagram(&input, filter);
     try std.testing.expectEqual(exp, input);
 }
 
@@ -269,6 +298,6 @@ test "test_empty" {
     var exp: [ex.len:0]u8 = undefined;
     std.mem.copy(u8, &input, in);
     std.mem.copy(u8, &exp, ex);
-    try makeAnagram(&input, filter);
+    makeAnagram(&input, filter);
     try std.testing.expectEqual(exp, input);
 }
